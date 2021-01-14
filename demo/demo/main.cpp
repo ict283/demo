b98eff507af79f5ff4d50634321b1d5d9b777814
ict283 <glennlum@idloud.com>
2021-01-14 21:28:24 +0800

    Update for PASS Session 1

---
+++ demo/demo/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  Demo
+
+/*
+ PASS_1 - Basic Project Setup for ICT283
+ 
+ Here is a basic C++ main program block.
+ In ICT283, we include the line 'using namespace std;'
+ to make coding easier for new students. This removes
+ the need for you to include namespaces in your code.
+ Note: This does not apply to all C++ code.
+ 
+ Eg.
+     Without 'using namespace std;'
+     std::cout<<"Hello World!"<<std::endl
+
+ This does not apply to all C++ code however.
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+    
+    
+    
+    //Your code here...
+    cout<<"Hello World!"<<endl;
+        
+    
+    
+    
+    return 0;
+}
