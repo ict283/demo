f46e8da21e0a84c55fd5f2ed5f7b6c370480401f
ict283 <glennlum@icloud.com>
2021-01-24 22:40:11 +0800

    Update for PASS Session 2

---
+++ demo/demo/Prime.cpp
@@ -0,0 +1,47 @@
+//
+//  Prime.cpp
+//  demo
+//
+//  Created by Glenn Lum on 24/1/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#include "Prime.h"
+
+Prime::Prime(){
+    prime_num=DEFAULT_PRIME;
+}
+
+Prime::~Prime(){}
+
+bool Prime::isPrime(const int n){
+    bool isPrime = true;
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1 ){
+        isPrime = false;
+    }else{
+        for (int i = 2; i <= n / 2; ++i){
+            if (n % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
+}
+
+bool Prime::setPrime(const int n){
+    
+    if(isPrime(n)){
+        prime_num=n;
+        return true;
+    }
+    else
+        return false;
+}
+
+int Prime::getPrime(){
+    return prime_num;
+}
+
+
---
+++ demo/demo/Prime.h
@@ -0,0 +1,26 @@
+//
+//  Prime.h
+//  demo
+//
+//  Created by Glenn Lum on 24/1/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#ifndef Prime_h
+#define Prime_h
+
+#include <stdio.h>
+
+const int DEFAULT_PRIME = 2;
+class Prime
+{
+public:
+    Prime();
+    ~Prime();
+    bool isPrime(const int n);
+    bool setPrime(const int n);
+    int getPrime();
+//private: /* Comment out for unit test */
+    int prime_num;
+};
+#endif /* Prime_h */
